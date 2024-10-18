d21d803493930bef6a13e82c8bf6801225b26182
zhenkaiwong1996 <zhenkaiwong.work@gmail.com>
2024-10-19 04:03:06 +0800

    feat: implement logging service

---
+++ Logging/BaseLogger.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace GithubActivity.Cli.Logging;
+
+public abstract class BaseLogger
+{
+  protected bool IsDebugMode()
+  {
+    return true;
+  }
+  public abstract void Info(string message);
+  public abstract void Error(string message);
+  public abstract void Debug(string message);
+}
---
+++ Logging/ConsoleLogger.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace GithubActivity.Cli.Logging;
+
+public class ConsoleLogger : BaseLogger
+{
+  public override void Debug(string message)
+  {
+    if (!IsDebugMode())
+    {
+      return;
+    }
+
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"DEBUG: {message}");
+    Console.ResetColor();
+  }
+
+  public override void Error(string message)
+  {
+
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"ERROR: {message}");
+    Console.ResetColor();
+  }
+
+  public override void Info(string message)
+  {
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"INFO: {message}");
+    Console.ResetColor();
+  }
+}
