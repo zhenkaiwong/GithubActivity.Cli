e27d6c2b98bc075a1052927d9d0b3e3bb3242648
zhenkaiwong1996 <zhenkaiwong.work@gmail.com>
2024-11-04 21:15:53 +0800

    feat: implement args processing and data output

--- Program.cs
+++ Program.cs
@@ -1,9 +1,51 @@
-﻿namespace GithubActivity.Cli;
+﻿using GithubActivity.Cli.Logging;
+using GithubActivity.Cli.Models.Events;
+using GithubActivity.Cli.Services;
+
+namespace GithubActivity.Cli;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        BaseLogger _logger = new ConsoleLogger();
+        if (!args.Length.Equals(1))
+        {
+            _logger.Error("Unexpected parameters found. You must pass a GitHub username as args");
+            return;
+        }
+
+        // if program args pass the check, we assume the only param is username
+        // api service should fail if the only param is not a valid username
+        string username = args[0];
+        var apiService = new GithubApiService(username, _logger);
+        List<BaseEvent> events = new();
+
+
+        try
+        {
+            events = await apiService.GetEventsAsync();
+        }
+        catch (HttpRequestException hrex)
+        {
+            _logger.Error($"{hrex.Message}");
+            _logger.Error($"Fail to find events for user {username}, user might not exist");
+            return;
+        }
+
+        if (events.Count == 0)
+        {
+            _logger.Error($"Unable to find events for user: {username}");
+            return;
+        }
+
+        foreach (BaseEvent @event in events)
+        {
+            string? activityString = @event.GetActivityString();
+            if (activityString != null)
+            {
+                _logger.Info(activityString);
+            }
+        }
     }
 }
