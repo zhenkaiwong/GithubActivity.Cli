277d1501ca4e21c39fdeb4ff3d5b67f50697c736
zhenkaiwong1996 <zhenkaiwong.work@gmail.com>
2024-11-04 19:57:01 +0800

    feat: implement custom converter to handle dynamic data structure of payload property from GitHub API response

---
+++ Json/GithubApiJsonConverter.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using GithubActivity.Cli.Json.Mappers;
+using GithubActivity.Cli.Logging;
+using GithubActivity.Cli.Models.Events;
+
+namespace GithubActivity.Cli.Json;
+
+public class GithubApiJsonConverter : JsonConverter<List<BaseEvent>>
+{
+  protected BaseLogger _logger = new ConsoleLogger();
+  public override List<BaseEvent> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+  {
+    List<BaseEvent> events = new();
+    using JsonDocument document = JsonDocument.ParseValue(ref reader);
+    JsonElement root = document.RootElement;
+    if (root.ValueKind != JsonValueKind.Array)
+    {
+      return events;
+    }
+
+    var _mapper = new AggregatedMapper();
+
+    foreach (JsonElement eventElement in root.EnumerateArray())
+    {
+      var mapFromJsonSuccess = _mapper.TryMapFromJsonElement(eventElement, out BaseEvent? result, out string error);
+      if (!mapFromJsonSuccess || result is null)
+      {
+        _logger.Error(error);
+        continue;
+      }
+
+      events.Add(result);
+    }
+
+    return events;
+  }
+
+  public override void Write(Utf8JsonWriter writer, List<BaseEvent> value, JsonSerializerOptions options)
+  {
+    _logger.Error("This converter isn't designed to deseralize object to JSON");
+    throw new NotImplementedException();
+  }
+}
